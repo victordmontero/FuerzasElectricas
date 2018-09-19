238d822fcd856ddf63116ef23ae621e01f6e0fb5
victordmontero <victordanielmontero@gmail.com>
2018-09-19 15:01:56 -0400

    Completando (No probado)

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
 #include "Vector3D.h"
 
 #define Ke 8987539422
@@ -9,17 +10,13 @@ using namespace std;
 
 struct Carga
 {
-	double x;
-	double y;
-	double z;
+	Vector3D pos;
 	double carga;
 };
 
 struct Fuerza
 {
-	double x;
-	double y;
-	double z;
+	Vector3D pos;
 	double magnitud;
 };
 
@@ -27,13 +24,13 @@ char ObtenerOpciones();
 void IntroducirCargas(vector<Carga> &cargas);
 void VerCargas(vector<Carga> &cargas);
 void ObtenerFuerzas(vector<Fuerza> &fuerzas, const vector<Carga> &cargas);
-double ObtenerDistancia(double x1, double y1, double z1, double x2, double y2, double z2);
 
 int main()
 {
 	Vector3D vec1(1, 1, 1);
-	Vector3D vec2(1, 1, 1);
+	Vector3D vec2(1, 2, 3);
 	vector<Carga> cargas;
+	vector<Fuerza> fuerzas;
 	bool isRunning = true;
 	while (isRunning)
 	{
@@ -53,7 +50,16 @@ int main()
 			break;
 
 		case 3:
-			//TODO Fuerzas
+			fuerzas.clear();
+			ObtenerFuerzas(fuerzas, cargas);
+			break;
+
+		case 4:
+			for (size_t i = 0; i < fuerzas.size(); i++)
+			{
+				cout << fuerzas[i].magnitud << " : " <<
+					fuerzas[i].pos.toString() << endl;
+			}
 			break;
 
 		default:
@@ -66,12 +72,13 @@ char ObtenerOpciones()
 {
 	int opc = 0;
 	cout << string(47, '-') << endl
-		 << "FuerzasElectricas por Victor D. Montero (18-09-2018)" << endl;
+		<< "FuerzasElectricas por Victor D. Montero (18-09-2018)" << endl;
 	cout << "1-Introducir Cargas" << endl
-		 << "2-Ver Cargas" << endl
-		 << "3-Obtener Fuerzas" << endl
-		 << "0-Salir" << endl
-		 << "-> ";
+		<< "2-Ver Cargas" << endl
+		<< "3-Obtener Fuerzas" << endl
+		<< "4-Imprimir Fuerzas" << endl
+		<< "0-Salir" << endl
+		<< "-> ";
 	cin >> opc;
 
 	return opc;
@@ -83,9 +90,9 @@ void IntroducirCargas(vector<Carga> &cargas)
 	while (true)
 	{
 		Carga carga;
-		cout << "Para salir entre carga 0" << endl;
+		cout << "Para salir entre \"0 0 0 0\"" << endl;
 		cout << "x,y,z-> ";
-		cin >> carga.x >> carga.y >> carga.z;
+		cin >> carga.pos.x >> carga.pos.y >> carga.pos.z;
 		cout << "carga-> ";
 		cin >> carga.carga;
 
@@ -99,23 +106,33 @@ void IntroducirCargas(vector<Carga> &cargas)
 void VerCargas(vector<Carga> &cargas)
 {
 	cout << string(47, '-') << endl;
-	for (int i = 0; i < cargas.size(); i++)
+	for (size_t i = 0; i < cargas.size(); i++)
 	{
 		Carga carga = cargas[i];
-		cout << "x=" << carga.x << " y=" << carga.y << " z=" << carga.z
-			 << " C=" << carga.carga << endl;
+		cout << "x=" << carga.pos.x << " y=" << carga.pos.y << " z=" << carga.pos.z
+			<< " C=" << carga.carga << endl;
 	}
 }
 
 void ObtenerFuerzas(vector<Fuerza> &fuerzas, const vector<Carga> &cargas)
 {
-	char S = 0;
-	for (size_t i = 0; i < cargas.size() - 1; i++)
+	cout << string(47, '-') << endl;
+	for (size_t i = 0; i < cargas.size(); i++)
 	{
-		for (size_t j = 0; j < cargas.size() - 1; j++)
+		for (size_t j = 0; j < cargas.size(); j++)
 		{
-			// Fuerza F;
-			// double magnitud = Ke * (cargas[i].carga * cargas[j].carga) /
+			if (i != j){
+				Fuerza F;
+				double distance = cargas.at(i).pos.squareDistanteTo(cargas.at(j).pos);
+				F.magnitud = Ke * (cargas.at(i).carga * cargas.at(j).carga) / distance;
+				double fx = 1;
+				double fy = 1;
+				double fz = 0.7071067812;
+
+				F.pos = Vector3D(acos(fx), asin(fy), asin(fz))*F.magnitud;
+
+				fuerzas.push_back(F);
+			}
 		}
 	}
 }
--- src/Vector3D.cpp
+++ src/Vector3D.cpp
@@ -2,143 +2,145 @@
 
 Vector3D Vector3D::operator+(const Vector3D &right) const
 {
-    return Vector3D(x + right.x, y + right.y, z + right.z);
+	return Vector3D(x + right.x, y + right.y, z + right.z);
 }
 
 Vector3D Vector3D::operator-(const Vector3D &right) const
 {
-    return Vector3D(x - right.x, y - right.y, z - right.z);
+	return Vector3D(x - right.x, y - right.y, z - right.z);
 }
 
 Vector3D Vector3D::operator*(double right) const
 {
-    return Vector3D(x * right, y * right, z * right);
+	return Vector3D(x * right, y * right, z * right);
 }
 
 Vector3D operator*(double left, const Vector3D &right)
 {
-    return right * left;
+	return right * left;
 }
 
 Vector3D Vector3D::operator/(double right) const
 {
-    return Vector3D(x / right, y / right, z / right);
+	return Vector3D(x / right, y / right, z / right);
 }
 
 Vector3D operator/(double left, const Vector3D &right)
 {
-    return right / left;
+	return right / left;
 }
 
 Vector3D Vector3D::operator-() const
 {
-    return Vector3D(-x, -y, -z);
+	return Vector3D(-x, -y, -z);
 }
 
 Vector3D &Vector3D::operator+=(const Vector3D &right)
 {
-    this->x += right.x;
-    this->y += right.y;
-    this->z += right.z;
-    return *this;
+	this->x += right.x;
+	this->y += right.y;
+	this->z += right.z;
+	return *this;
 }
 
 Vector3D &Vector3D::operator-=(const Vector3D &right)
 {
-    this->x -= right.x;
-    this->y -= right.y;
-    this->z -= right.z;
-    return *this;
+	this->x -= right.x;
+	this->y -= right.y;
+	this->z -= right.z;
+	return *this;
 }
 
 Vector3D &Vector3D::operator*=(double right)
 {
-    this->x *= right;
-    this->y *= right;
-    this->z *= right;
-    return *this;
+	this->x *= right;
+	this->y *= right;
+	this->z *= right;
+	return *this;
 }
 
 Vector3D &Vector3D::operator/=(double right)
 {
-    this->x /= right;
-    this->y /= right;
-    this->z /= right;
-    return *this;
+	this->x /= right;
+	this->y /= right;
+	this->z /= right;
+	return *this;
 }
 
 Vector3D &Vector3D::operator=(const Vector3D &right)
 {
-    x = right.x;
-    y = right.y;
-    z = right.z;
+	x = right.x;
+	y = right.y;
+	z = right.z;
 
-    return *this;
+	return *this;
 }
 
 bool Vector3D::operator==(const Vector3D &right)
 {
-    return right.x == x && right.y == y && right.z == z;
+	return right.x == x &&
+		   right.y == y &&
+		   right.z == z;
 }
 
 bool Vector3D::operator!=(const Vector3D &right)
 {
-    return !(*this == right);
+	return !(*this == right);
 }
 
 bool Vector3D::operator<(const Vector3D &right)
 {
-    return this->x < right.x &&
-           this->y < right.y &&
-           this->z < right.z;
+	return this->x < right.x &&
+		   this->y < right.y &&
+		   this->z < right.z;
 }
 
 bool Vector3D::operator>(const Vector3D &right)
 {
-    return this->x > right.x &&
-           this->y > right.y &&
-           this->z > right.z;
+	return this->x > right.x &&
+		   this->y > right.y &&
+		   this->z > right.z;
 }
 
 double Vector3D::dotProduct(const Vector3D &vec) const
 {
-    return vec.x * x + vec.y * y + vec.z * z;
+	return vec.x * x + vec.y * y + vec.z * z;
 }
 
 double Vector3D::squareLength() const
 {
-    return dotProduct(*this);
+	return dotProduct(*this);
 }
 
 double Vector3D::squareDistanteTo(const Vector3D &vec) const
 {
-    return (*this - vec).squareLength();
+	return (*this - vec).squareLength();
 }
 
 std::string Vector3D::toString() const
 {
-    std::string result;
-    char buffer[512 + 1] = {0};
+	std::string result;
+	char buffer[512 + 1] = {0};
 
-    // std::cout << x << std::endl;
-    result.push_back('(');
-    sprintf(buffer, "%lf", x);
-    result += buffer;
-    result.push_back(',');
-    memset(buffer, 0, 513);
+	// std::cout << x << std::endl;
+	result.push_back('(');
+	sprintf(buffer, "%lf", x);
+	result += buffer;
+	result.push_back(',');
+	memset(buffer, 0, 513);
 
-    // std::cout << y << std::endl;
-    sprintf(buffer, "%lf", y);
-    result += buffer;
-    result.push_back(',');
-    memset(buffer, 0, 513);
+	// std::cout << y << std::endl;
+	sprintf(buffer, "%lf", y);
+	result += buffer;
+	result.push_back(',');
+	memset(buffer, 0, 513);
 
-    // std::cout << z << std::endl;
-    sprintf(buffer, "%lf", z);
-    result += buffer;
-    result.push_back(')');
-    memset(buffer, 0, 513);
+	// std::cout << z << std::endl;
+	sprintf(buffer, "%lf", z);
+	result += buffer;
+	result.push_back(')');
+	memset(buffer, 0, 513);
 
-    // std::cout << result.c_str() << std::endl;
-    return result;
+	// std::cout << result.c_str() << std::endl;
+	return result;
 }
